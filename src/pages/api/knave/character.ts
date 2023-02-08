5b361c138a3907710a3de7f17e4918b69470eaf3
John Riccardi <riccjohn@gmail.com>
2023-02-08 15:55:42 -0600

    Create /api/knave/character route

---
+++ src/pages/api/knave/character.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { KnaveCharacter } from "@/knave";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    const character = new KnaveCharacter()
+    character.generate()
+    res.status(200).json(character)
+  } else {
+    res.status(400)
+  }
+}
+
+export default handler
