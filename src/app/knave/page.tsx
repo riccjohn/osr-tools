2fba2a945e8a26bf27b8eee1e9fe37c24a2ca427
John Riccardi <riccjohn@gmail.com>
2023-02-09 12:29:55 -0600

    Install TailwindCSS

---
+++ src/app/knave/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Knave: NextPage = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello, Next.js!
+    </h1>
+  )
+}
+
+export default Knave
