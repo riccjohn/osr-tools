9c75452ee1f96bb59e1cb455e9d280b94c52560e
John Riccardi <riccjohn@gmail.com>
2023-02-13 16:55:24 -0600

    11 Remove default styles, add basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="container mx-auto flex flex-col bg-white dark:bg-slate-900">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
