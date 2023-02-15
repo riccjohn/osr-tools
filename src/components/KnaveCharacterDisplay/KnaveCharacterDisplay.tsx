4d06f92faa050c222ae5551517a3b9220e51f042
John Riccardi <riccjohn@gmail.com>
2023-02-15 09:23:32 -0600

    11 WIP Add styling

--- src/components/KnaveCharacterDisplay/KnaveCharacterDisplay.tsx
+++ src/components/KnaveCharacterDisplay/KnaveCharacterDisplay.tsx
@@ -14,7 +14,7 @@ const KnaveCharacterDisplay: React.FC<IKnaveCharacterProps> = ({
     <div className='container mx-auto'>
       {character && (
         <div className='container mx-auto flex flex-col px-1 text-black dark:text-white'>
-          <section className='flex flex-col py-2'>
+          <section className='flex flex-col py-2 items-center'>
             <div className='flex'>
               <p className='mr-1'>
                 <span className='font-bold'>HP: </span>
@@ -36,73 +36,98 @@ const KnaveCharacterDisplay: React.FC<IKnaveCharacterProps> = ({
 }
 
 const Traits: React.FC<{ traits: ITraits }> = ({ traits }) => {
+  const traitNames = Object.keys(traits)
+
+  const traitsColumnOne = traitNames.slice(0, traitNames.length / 2)
+  const traitsColumnTwo = traitNames.slice(traitNames.length / 2)
+
   return (
-    <section className='flex flex-col py-2'>
+    <section className='flex w-full flex-col items-center py-2'>
       <h2 className='text-center text-lg font-bold'>Traits</h2>
-      <div className='text-base'>
-        {Object.keys(traits).map((trait: string) => {
-          return (
-            <p key={trait} data-testid={trait}>
-              <span className='font-bold capitalize'>{`${trait}: `}</span>
-              <span className='text-base capitalize'>
-                {traits[trait as INoun]}
-              </span>
-            </p>
-          )
-        })}
+      <div className='flex w-1/2 items-center justify-between'>
+        <div className='w-fit'>
+          {traitsColumnOne.map(traitName => (
+            <Trait
+              key={traitName}
+              name={traitName}
+              description={traits[traitName as INoun]}
+            />
+          ))}
+        </div>
+        <div className='w-fit'>
+          {traitsColumnTwo.map(traitName => (
+            <Trait
+              key={traitName}
+              name={traitName}
+              description={traits[traitName as INoun]}
+            />
+          ))}
+        </div>
       </div>
     </section>
   )
 }
 
+const Trait: React.FC<{ name: string; description: string }> = ({
+  name,
+  description,
+}) => (
+  <p data-testid={name} className='w-fit'>
+    <span className='font-bold capitalize'>{`${name}: `}</span>
+    <span className='text-base capitalize'>{description}</span>
+  </p>
+)
+
 const AbilityScores: React.FC<{ abilityScores: IAbilities }> = ({
   abilityScores,
 }) => {
   return (
-    <section className='flex flex-col py-2'>
+    <section className='flex w-full flex-col items-center py-2'>
       <h2 className='text-center text-lg font-bold'>Abilities</h2>
 
-      <table className='max-w-xl'>
-        <thead>
-          <tr role='row'>
-            <th role='cell'>Defense</th>
-            <th role='cell'>Ability</th>
-            <th role='cell'>Bonus</th>
-          </tr>
-        </thead>
-        <tbody>
-          <AbilityRow
-            label={'STR'}
-            bonus={abilityScores.strength.bonus}
-            defense={abilityScores.strength.defense}
-          />
-          <AbilityRow
-            label='CON'
-            bonus={abilityScores.constitution.bonus}
-            defense={abilityScores.constitution.defense}
-          />
-          <AbilityRow
-            label='DEX'
-            bonus={abilityScores.dexterity.bonus}
-            defense={abilityScores.dexterity.defense}
-          />
-          <AbilityRow
-            label='INT'
-            bonus={abilityScores.intelligence.bonus}
-            defense={abilityScores.intelligence.defense}
-          />
-          <AbilityRow
-            label='WIS'
-            bonus={abilityScores.wisdom.bonus}
-            defense={abilityScores.wisdom.defense}
-          />
-          <AbilityRow
-            label='CHA'
-            bonus={abilityScores.charisma.bonus}
-            defense={abilityScores.charisma.defense}
-          />
-        </tbody>
-      </table>
+      <div className='w-1/2'>
+        <table className='w-full border-collapse'>
+          <thead>
+            <tr role='row'>
+              <th role='cell'>Defense</th>
+              <th role='cell'>Ability</th>
+              <th role='cell'>Bonus</th>
+            </tr>
+          </thead>
+          <tbody>
+            <AbilityRow
+              label={'STR'}
+              bonus={abilityScores.strength.bonus}
+              defense={abilityScores.strength.defense}
+            />
+            <AbilityRow
+              label='CON'
+              bonus={abilityScores.constitution.bonus}
+              defense={abilityScores.constitution.defense}
+            />
+            <AbilityRow
+              label='DEX'
+              bonus={abilityScores.dexterity.bonus}
+              defense={abilityScores.dexterity.defense}
+            />
+            <AbilityRow
+              label='INT'
+              bonus={abilityScores.intelligence.bonus}
+              defense={abilityScores.intelligence.defense}
+            />
+            <AbilityRow
+              label='WIS'
+              bonus={abilityScores.wisdom.bonus}
+              defense={abilityScores.wisdom.defense}
+            />
+            <AbilityRow
+              label='CHA'
+              bonus={abilityScores.charisma.bonus}
+              defense={abilityScores.charisma.defense}
+            />
+          </tbody>
+        </table>
+      </div>
     </section>
   )
 }
@@ -115,7 +140,11 @@ interface IAbilityRowProps {
 
 const AbilityRow: React.FC<IAbilityRowProps> = ({ bonus, defense, label }) => {
   return (
-    <tr role='row' data-testid={`${label.toLowerCase()}-row`}>
+    <tr
+      role='row'
+      data-testid={`${label.toLowerCase()}-row`}
+      className='w-fit text-center'
+    >
       <td role='cell'>{defense}</td>
       <th role='cell'>{label}</th>
       <td role='cell'>{bonus}</td>
@@ -128,34 +157,36 @@ const Items: React.FC<IKnaveCharacterProps> = ({ character }) => {
   const itemSlotsUsed = items.reduce((acc, curr) => acc + curr.slots, 0)
 
   return (
-    <section className='flex flex-col py-2'>
+    <section className='flex w-full flex-col items-center py-2'>
       <h2 className='text-center text-lg font-bold'>Weapons & Gear</h2>
       <p data-testid='item-slots-used'>
         {`Item Slots Used: ${itemSlotsUsed}/${itemSlots}`}
       </p>
-      <table>
-        <thead>
-          <tr role='row'>
-            <th role='cell' data-testid='item-name-heading'>
-              Item
-            </th>
-            <th role='cell' data-testid='item-defense-heading'>
-              Defense
-            </th>
-            <th role='cell' data-testid='item-damage-heading'>
-              Damage
-            </th>
-            <th role='cell' data-testid='item-slots-heading'>
-              Slots
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item, idx) => (
-            <ItemRow item={item} key={`${item.name}-${idx}`} />
-          ))}
-        </tbody>
-      </table>
+      <div className='w-1/2'>
+        <table className='w-full'>
+          <thead>
+            <tr role='row' className='text-center'>
+              <th role='cell' data-testid='item-name-heading'>
+                Item
+              </th>
+              <th role='cell' data-testid='item-defense-heading'>
+                Defense
+              </th>
+              <th role='cell' data-testid='item-damage-heading'>
+                Damage
+              </th>
+              <th role='cell' data-testid='item-slots-heading'>
+                Slots
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, idx) => (
+              <ItemRow item={item} key={`${item.name}-${idx}`} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </section>
   )
 }
@@ -167,11 +198,23 @@ interface IItemRowProps {
 const ItemRow: React.FC<IItemRowProps> = ({ item }) => {
   const hyphenatedItemName = item.name.split(' ').join('-')
   return (
-    <tr role='row' data-testid={`${hyphenatedItemName}-row`}>
-      <td role='cell' data-testid='item-name'>{item.name}</td>
-      <td role='cell' data-testid='item-defense'>{item.defense ? item.defense : '-'}</td>
-      <td role='cell' data-testid='item-damage'>{item.damage ? item.damage : '-'}</td>
-      <td role='cell' data-testid='item-slots'>{item.slots}</td>
+    <tr
+      role='row'
+      data-testid={`${hyphenatedItemName}-row`}
+      className='text-center'
+    >
+      <td role='cell' data-testid='item-name'>
+        {item.name}
+      </td>
+      <td role='cell' data-testid='item-defense'>
+        {item.defense ? item.defense : '-'}
+      </td>
+      <td role='cell' data-testid='item-damage'>
+        {item.damage ? item.damage : '-'}
+      </td>
+      <td role='cell' data-testid='item-slots'>
+        {item.slots}
+      </td>
     </tr>
   )
 }
