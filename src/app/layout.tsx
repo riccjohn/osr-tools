a541e9ced0b94c9c9e8bbff0fe2ead3b4e38c288
John Riccardi <riccjohn@gmail.com>
2023-02-17 15:59:14 -0600

    Switch to tailwindui navbar menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import localFont from '@next/font/local';
+import { NavBar } from '@/components'
+import localFont from '@next/font/local'
 import './globals.css'
 
 const sebaldusGotisch = localFont({
@@ -7,12 +8,13 @@ const sebaldusGotisch = localFont({
   variable: '--font-sebaldusGotisch',
 })
 
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pages = [{ name: 'Knave', href: '/knave/generate-character' }]
+
   return (
     <html lang='en' className={sebaldusGotisch.variable}>
       {/*
@@ -21,7 +23,8 @@ export default function RootLayout({
       */}
       <head />
       <body>
-        <div className="container mx-auto flex flex-col bg-white dark:bg-slate-900">
+        <NavBar pages={pages} />
+        <div className='container mx-auto flex flex-col bg-white dark:bg-slate-900'>
           {children}
         </div>
       </body>
