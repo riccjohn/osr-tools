76696e88f7b5213aceff5ca7fc4ede1a13a38888
John Riccardi <riccjohn@gmail.com>
2023-02-17 16:35:22 -0600

    Add animated spinner, update README

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as KnaveCharacterDisplay } from '@/components/KnaveCharacterDisplay'
-
 export { default as NavBar } from '@/components/NavBar'
+export { default as Spinner } from '@/components/Spinner'
